5e47be1477b30a4d74cfd3e0ca685db30492de36
Kevz Macalalag <mc.kvnflrs@gmail.com>
2026-02-19 23:10:36 +0800

    base

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -1,24 +1,7 @@
 import React from 'react';
 
 function CardItem() {
-	return (
-		<div className='card card-side card-border bg-base-100 shadow-sm'>
-			<figure>
-				<img
-					src='https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp'
-					alt='Movie'
-				/>
-			</figure>
-			<div className='card-body'>
-				<h2 className='card-title'>item name</h2>
-				<p>Item description</p>
-				<div className='card-actions justify-end'>
-					<button className='btn btn-primary'>Buy</button>
-					<button className='btn btn-primary'>Sell</button>
-				</div>
-			</div>
-		</div>
-	);
+	return <div>CardItem</div>;
 }
 
 export default CardItem;
