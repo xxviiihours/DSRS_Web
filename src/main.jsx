5f952cbe07c86d36bec7e3af2ef2c7735ea1722b
Kevz Macalalag <mc.kvnflrs@gmail.com>
2026-02-20 22:00:50 +0800

    applied custom style to remove certain off themed color in recharts

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles/index.css';
+import './styles/custom.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
