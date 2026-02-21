a35bc781a44dc9917c02ab66a0e71612282d305f
Kevz Macalalag <mc.kvnflrs@gmail.com>
2026-02-21 19:08:27 +0800

    more styling

--- src/features/buyers/ui/RecentBuyers.jsx
+++ src/features/buyers/ui/RecentBuyers.jsx
@@ -3,21 +3,22 @@ import React from 'react';
 function RecentBuyers() {
 	return (
 		<div className='card bg-base-100 col-span-1 md:col-span-2 lg:col-span-1 shadow-sm w-auto content-center'>
-			<div className='card-body p-0 pt-2'>
+			<div className='card-body p-0 pt-4'>
 				<h2 className='card-title justify-center mb-4'>RECENT BUYERS</h2>
-				<ul className='list'>
+				<ul className='list overflow-auto h-70'>
 					<li className='list-row'>
-						<div className=''>
+						<div className='list-col-grow'>
 							<img
-								className='size-10 rounded-box'
+								className='size-12 rounded-box'
 								src='https://img.daisyui.com/images/profile/demo/1@94.webp'
 							/>
-						</div>
-						<div className='list-col-grow'>
-							<div className='font-semibold'>Jessi Tunderg</div>
+							<div className=' font-semibold'>Zesty Six Seven</div>
 							<div className='opacity-60 text-xs'>$100.00</div>
 						</div>
-						<div className='text-xs text-error'>↗︎ (22%)</div>
+						<div className='grid grid-cols-1 grid-rows-1'>
+							<p className='text-xs text-error text-right'>↗︎ (22%)</p>
+							<button className='btn btn-info btn-soft btn-block'>View</button>
+						</div>
 					</li>
 				</ul>
 			</div>
