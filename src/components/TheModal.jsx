542d767e1b95aa6078832af10e8937338c11870c
Kevz Macalalag <mc.kvnflrs@gmail.com>
2026-02-24 01:05:39 +0800

    removed unused effect in modal

--- src/components/TheModal.jsx
+++ src/components/TheModal.jsx
@@ -1,12 +1,9 @@
 import React, { useEffect } from 'react';
 
-function TheModal({ show, children }) {
-	useEffect(() => {
-		if (show) document.getElementById('my_modal_2').showModal();
-	}, [show]);
+function TheModal({ children }) {
 	return (
-		<dialog id='my_modal_2' className='modal'>
-			<div className='modal-box h-80 flex flex-col justify-center items-center text-center'>
+		<dialog id='my_modal_2' className='modal modal-open'>
+			<div className='modal-box h-100 flex flex-col justify-center items-center text-center'>
 				{children}
 			</div>
 
