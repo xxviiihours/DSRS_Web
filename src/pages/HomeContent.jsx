1d015ddcabdf998ee68b5e224683e10df40f39a6
Kevz Macalalag <mc.kvnflrs@gmail.com>
2026-02-24 01:08:34 +0800

    Add player check (subject to change) moved static data to  temData.js

--- src/pages/HomeContent.jsx
+++ src/pages/HomeContent.jsx
@@ -1,61 +1,29 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { BaseLayout, ContentLayout } from '../layout';
 import TheChart from '../components/TheChart';
 import ItemMain from '../features/item/ui/ItemMain';
 import RecentBuyers from '../features/buyers/ui/RecentBuyers';
 import RecentSellers from '../features/sellers/ui/RecentSellers';
+import { data as TempChartData } from '../temp/tempData';
+import { useSelector } from 'react-redux';
+import { useInitDailyPricesQuery } from '../features/market/api/marketApi';
 
 function HomeContent() {
-	const data = [
-		{
-			name: 'Page A',
-			uv: 5400,
-			pv: -2400,
-			amt: 2400,
-		},
-		{
-			name: 'Page B',
-			uv: 4300,
-			pv: -4567,
-			amt: 2400,
-		},
-		{
-			name: 'Page A',
-			uv: 1400,
-			pv: -2400,
-			amt: 2400,
-		},
-		{
-			name: 'Page B',
-			uv: 3200,
-			pv: -4567,
-			amt: 2400,
-		},
-		{
-			name: 'Page A',
-			uv: 1400,
-			pv: -2400,
-			amt: 2400,
-		},
-		{
-			name: 'Page B',
-			uv: 3200,
-			pv: -4567,
-			amt: 2400,
-		},
-	];
-
+	const player = useSelector((state) => state.player);
+	const { data, isFetching } = useInitDailyPricesQuery({ id: player.id });
 	return (
-		<BaseLayout>
-			<ContentLayout>
-				<div className='grid grid-rows-3 lg:grid-rows-1 grid-cols-1 md:grid-cols-4 lg:grid-cols-4 gap-2'>
-					<TheChart data={data} />
-					<ItemMain />
-					<RecentBuyers />
-					<RecentSellers />
-				</div>
-			</ContentLayout>
-		</BaseLayout>
+		<>
+			<BaseLayout>
+				<ContentLayout>
+					<div className='grid grid-rows-3 lg:grid-rows-1 grid-cols-1 md:grid-cols-4 lg:grid-cols-4 gap-2'>
+						<TheChart data={TempChartData} isFetching={isFetching} />
+						<ItemMain prices={data?.dailyPrices} isFetching={isFetching} />
+						<RecentBuyers isFetching={isFetching} />
+						<RecentSellers isFetching={isFetching} />
+					</div>
+				</ContentLayout>
+			</BaseLayout>
+		</>
 	);
 }
 
---
+++ src/temp/tempData.js
@@ -0,0 +1,38 @@
+export const data = [
+	{
+		name: 'Page A',
+		uv: 5400,
+		pv: -2400,
+		amt: 2400,
+	},
+	{
+		name: 'Page B',
+		uv: 4300,
+		pv: -4567,
+		amt: 2400,
+	},
+	{
+		name: 'Page A',
+		uv: 1400,
+		pv: -2400,
+		amt: 2400,
+	},
+	{
+		name: 'Page B',
+		uv: 3200,
+		pv: -4567,
+		amt: 2400,
+	},
+	{
+		name: 'Page A',
+		uv: 1400,
+		pv: -2400,
+		amt: 2400,
+	},
+	{
+		name: 'Page B',
+		uv: 3200,
+		pv: -4567,
+		amt: 2400,
+	},
+];
