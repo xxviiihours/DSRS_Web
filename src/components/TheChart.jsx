ba8f8843301e8babd6ca4e3dc3924d102edfa3ff
Kevz Macalalag <mc.kvnflrs@gmail.com>
2026-02-24 01:05:12 +0800

    Applied loading indicator to the home contents

--- src/components/TheChart.jsx
+++ src/components/TheChart.jsx
@@ -1,59 +1,64 @@
 import React from 'react';
 import { Bar, BarChart, CartesianGrid, Legend, Tooltip, XAxis, YAxis } from 'recharts';
 import { getDaisyUIColor } from '../utils/colorHelper';
+import TheLoader from './TheLoader';
 
-function TheChart({ data, options = null }) {
+function TheChart({ data, isFetching = true }) {
 	return (
 		<div className='card w-auto h-100 bg-base-100 row-start-1 col-start-1 col-span-full'>
 			<div className='card-body rechart-wrapper'>
 				<h2 className='card-title'>PRICE HISTORY</h2>
-				<BarChart
-					style={{
-						width: '100%',
-						aspectRatio: 1.718,
-						height: '300px',
-					}}
-					responsive
-					data={data}
-				>
-					<CartesianGrid
-						stroke={getDaisyUIColor('--color-base-content')}
-						strokeDasharray='5 5'
-					/>
-					<XAxis dataKey='name' stroke={getDaisyUIColor('--color-base-content')} />
-					<YAxis stroke={getDaisyUIColor('--color-base-content')} />
-					<Bar
-						activeBar={{ stroke: 'none', fillOpacity: 0.9 }}
-						dataKey='uv'
-						fill={getDaisyUIColor('--color-success')}
-						barSize={50}
-					/>
-					<Bar
-						activeBar={{ stroke: 'none', fillOpacity: 0.9 }}
-						dataKey='pv'
-						fill={getDaisyUIColor('--color-error')}
-						barSize={50}
-					/>
-					<Legend
-						align='top'
-						wrapperStyle={{
-							backgroundColor: 'transparent',
-							color: '#fff',
+				{isFetching ? (
+					<TheLoader />
+				) : (
+					<BarChart
+						style={{
+							width: '100%',
+							aspectRatio: 1.718,
+							height: '300px',
 						}}
-					/>
-					<Tooltip
-						cursor={{ fill: 'rgba(255,255,255,0.04)' }}
-						contentStyle={{
-							backgroundColor: getDaisyUIColor('--color-base-200'),
-							border: `1px solid ${getDaisyUIColor('--color-base-200')}`,
-							color: getDaisyUIColor('--color-base-content'),
-						}}
-						wrapperStyle={{
-							width: 100,
-							backgroundColor: `${getDaisyUIColor('--color-base-200')}`,
-						}}
-					/>
-				</BarChart>
+						responsive
+						data={data}
+					>
+						<CartesianGrid
+							stroke={getDaisyUIColor('--color-base-content')}
+							strokeDasharray='5 5'
+						/>
+						<XAxis dataKey='name' stroke={getDaisyUIColor('--color-base-content')} />
+						<YAxis stroke={getDaisyUIColor('--color-base-content')} />
+						<Bar
+							activeBar={{ stroke: 'none', fillOpacity: 0.9 }}
+							dataKey='uv'
+							fill={getDaisyUIColor('--color-success')}
+							barSize={50}
+						/>
+						<Bar
+							activeBar={{ stroke: 'none', fillOpacity: 0.9 }}
+							dataKey='pv'
+							fill={getDaisyUIColor('--color-error')}
+							barSize={50}
+						/>
+						<Legend
+							align='top'
+							wrapperStyle={{
+								backgroundColor: 'transparent',
+								color: '#fff',
+							}}
+						/>
+						<Tooltip
+							cursor={{ fill: 'rgba(255,255,255,0.04)' }}
+							contentStyle={{
+								backgroundColor: getDaisyUIColor('--color-base-200'),
+								border: `1px solid ${getDaisyUIColor('--color-base-200')}`,
+								color: getDaisyUIColor('--color-base-content'),
+							}}
+							wrapperStyle={{
+								width: 100,
+								backgroundColor: `${getDaisyUIColor('--color-base-200')}`,
+							}}
+						/>
+					</BarChart>
+				)}
 			</div>
 		</div>
 	);
--- src/features/sellers/ui/RecentSellers.jsx
+++ src/features/sellers/ui/RecentSellers.jsx
@@ -1,26 +1,31 @@
 import React from 'react';
+import TheLoaderSmall from '../../../components/TheLoaderSmall';
 
-function RecentSellers() {
+function RecentSellers({ sellers, isFetching = true }) {
 	return (
 		<div className='card bg-base-100 col-span-1 md:col-span-2 lg:col-span-1 shadow-sm w-auto'>
 			<div className='card-body p-0 pt-4'>
 				<h2 className='card-title justify-center mb-4'>RECENT SELLERS</h2>
-				<ul className='list overflow-auto h-70'>
-					<li className='list-row'>
-						<div className='list-col-grow'>
-							<img
-								className='size-12 rounded-box'
-								src='https://img.daisyui.com/images/profile/demo/1@94.webp'
-							/>
-							<div className=' font-semibold'>Zesty Six Seven</div>
-							<div className='opacity-60 text-xs'>$100.00</div>
-						</div>
-						<div className='grid grid-cols-1 grid-rows-1'>
-							<p className='text-xs text-error text-right'>↗︎ (22%)</p>
-							<button className='btn btn-info btn-soft btn-block'>View</button>
-						</div>
-					</li>
-				</ul>
+				{isFetching ? (
+					<TheLoaderSmall />
+				) : (
+					<ul className='list overflow-auto h-70'>
+						<li className='list-row'>
+							<div className='list-col-grow'>
+								<img
+									className='size-12 rounded-box'
+									src='https://img.daisyui.com/images/profile/demo/1@94.webp'
+								/>
+								<div className=' font-semibold'>Zesty Six Seven</div>
+								<div className='opacity-60 text-xs'>$100.00</div>
+							</div>
+							<div className='grid grid-cols-1 grid-rows-1'>
+								<p className='text-xs text-error text-right'>↗︎ (22%)</p>
+								<button className='btn btn-info btn-soft btn-block'>View</button>
+							</div>
+						</li>
+					</ul>
+				)}
 			</div>
 		</div>
 	);
