6b2ea1f13ea4250b194cf94a5c5655f0a3729f09
Kevz Macalalag <mc.kvnflrs@gmail.com>
2026-02-25 00:12:53 +0800

    Add item dispatcher upon component mount

--- src/features/item/ui/ItemMain.jsx
+++ src/features/item/ui/ItemMain.jsx
@@ -1,28 +1,37 @@
 import React, { useEffect, useRef, useState } from 'react';
 import TheLoaderInfo from '../../../components/TheLoaderInfo';
+import { useDispatch } from 'react-redux';
+import { setItem } from '../model/itemSlice';
 
-function ItemMain({ prices, isFetching = true }) {
+function ItemMain({ prices, isFetching }) {
+	const dispatch = useDispatch();
 	const carouselRef = useRef(null);
 	const [currentSlide, setCurrentSlide] = useState(0);
 
 	const totalSlides = prices?.length;
 
-	const goto = (price, index) => {
+	const goto = (index) => {
 		if (carouselRef.current) {
 			const width = carouselRef.current.offsetWidth;
 			carouselRef.current.scrollTo({
 				left: width * index,
 				behavior: 'smooth',
 			});
-
-			console.log(price);
 			setCurrentSlide(index);
 		}
+
+		dispatch(setItem(prices[index].item));
 	};
 
-	const nextItem = (price) => goto(price, (currentSlide + 1) % totalSlides);
-	const prevItem = (price) => goto(price, (currentSlide - 1 + totalSlides) % totalSlides);
+	const nextItem = () => goto((currentSlide + 1) % totalSlides);
+	const prevItem = () => goto((currentSlide - 1 + totalSlides) % totalSlides);
 
+	useEffect(() => {
+		if (prices?.length > 0) {
+			console.log('triggered');
+			dispatch(setItem(prices[0].item));
+		}
+	}, [prices, dispatch]);
 	return (
 		<div
 			ref={carouselRef}
@@ -53,6 +62,7 @@ function ItemMain({ prices, isFetching = true }) {
 										{price.state === 1 && <div className='badge badge-error ml-2'>HIGH</div>}
 									</h2>
 									<h3 className='opacity-60'>Base Price: ${price.item.basePrice}</h3>
+									<h3 className='font-semibold'>{price.item.name}</h3>
 									<p className='italic'>{price.item.description}</p>
 									<div className='card-actions justify-end mt-4'>
 										<button className='btn btn-success btn-soft'>Buy</button>
@@ -64,12 +74,12 @@ function ItemMain({ prices, isFetching = true }) {
 					</div>
 
 					{/* Navigation buttons inside the card */}
-					<div className='absolute flex justify-between transform -translate-y-1/2 left-0 right-0 top-1/3'>
+					<div className='absolute flex justify-between transform -translate-y-1/2 left-0 right-0 top-1/3 p-2'>
 						<div className='absolute top-1 left-2 right-2 flex justify-between transform -translate-y-1/2'>
-							<button onClick={() => prevItem(price)} className='btn btn-circle'>
+							<button onClick={prevItem} className='btn btn-circle'>
 								❮
 							</button>
-							<button onClick={() => nextItem(price)} className='btn btn-circle'>
+							<button onClick={nextItem} className='btn btn-circle'>
 								❯
 							</button>
 						</div>
