065f12d60f87bab7940ea97fb5c69e6029c63b71
Kevz Macalalag <mc.kvnflrs@gmail.com>
2026-02-25 00:13:29 +0800

    Applied dashboard api to load previous prices in chart for specific item

--- src/components/TheChart.jsx
+++ src/components/TheChart.jsx
@@ -2,13 +2,31 @@ import React from 'react';
 import { Bar, BarChart, CartesianGrid, Legend, Tooltip, XAxis, YAxis } from 'recharts';
 import { getDaisyUIColor } from '../utils/colorHelper';
 import TheLoader from './TheLoader';
+import { useGetDailyPricesByItemQuery } from '../features/dashboard/api/dashboardApi';
+import { skipToken } from '@reduxjs/toolkit/query';
 
-function TheChart({ data, isFetching = true }) {
+function TheChart({ player, item }) {
+	const { data, isLoading } = useGetDailyPricesByItemQuery(
+		item?.id && player?.id ? { itemId: item.id, playerId: player.id } : skipToken,
+	);
+	const basePrice = item?.basePrice;
+
+	const chartData = data?.map((item) => {
+		const diff = item.previousPrice - basePrice;
+
+		return {
+			date: item.date,
+			value: diff,
+			originalPrice: item.previousPrice,
+			fill: diff >= 0 ? getDaisyUIColor('--color-success') : getDaisyUIColor('--color-error'),
+		};
+	});
+
 	return (
 		<div className='card w-auto h-100 bg-base-100 row-start-1 col-start-1 col-span-full'>
 			<div className='card-body rechart-wrapper'>
 				<h2 className='card-title'>PRICE HISTORY</h2>
-				{isFetching ? (
+				{isLoading ? (
 					<TheLoader />
 				) : (
 					<BarChart
@@ -18,25 +36,22 @@ function TheChart({ data, isFetching = true }) {
 							height: '300px',
 						}}
 						responsive
-						data={data}
+						data={chartData}
 					>
 						<CartesianGrid
 							stroke={getDaisyUIColor('--color-base-content')}
 							strokeDasharray='5 5'
 						/>
-						<XAxis dataKey='name' stroke={getDaisyUIColor('--color-base-content')} />
-						<YAxis stroke={getDaisyUIColor('--color-base-content')} />
-						<Bar
-							activeBar={{ stroke: 'none', fillOpacity: 0.9 }}
-							dataKey='uv'
-							fill={getDaisyUIColor('--color-success')}
-							barSize={50}
+						<XAxis dataKey='date' stroke={getDaisyUIColor('--color-base-content')} />
+						<YAxis
+							domain={['auto', 'auto']}
+							tickFormatter={(v) => basePrice + v}
+							stroke={getDaisyUIColor('--color-base-content')}
 						/>
 						<Bar
-							activeBar={{ stroke: 'none', fillOpacity: 0.9 }}
-							dataKey='pv'
-							fill={getDaisyUIColor('--color-error')}
-							barSize={50}
+							dataKey='value'
+							name={'Price Difference'}
+							fill={getDaisyUIColor('--color-base-content')}
 						/>
 						<Legend
 							align='top'
@@ -53,7 +68,7 @@ function TheChart({ data, isFetching = true }) {
 								color: getDaisyUIColor('--color-base-content'),
 							}}
 							wrapperStyle={{
-								width: 100,
+								width: 'auto',
 								backgroundColor: `${getDaisyUIColor('--color-base-200')}`,
 							}}
 						/>
