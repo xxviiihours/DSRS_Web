f7b107864ece5a6ba8b855d05561ceb7cf37232d
Kevz Macalalag <mc.kvnflrs@gmail.com>
2026-02-26 02:11:48 +0800

    removed unused imports

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles/index.css';
 import './styles/custom.css';
+import './styles/global.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
--- src/pages/HomeContent.jsx
+++ src/pages/HomeContent.jsx
@@ -1,14 +1,12 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { BaseLayout, ContentLayout } from '../layout';
-import TheChart from '../components/TheChart';
 import { useInitDailyPricesQuery } from '../features/market/api/marketApi';
 import ItemMain from '../features/item/ui/ItemMain';
 import RecentBuyers from '../features/buyers/ui/RecentBuyers';
 import RecentSellers from '../features/sellers/ui/RecentSellers';
-import { data as TempChartData } from '../temp/tempData';
 import { useSelector } from 'react-redux';
-import { useGetDailyPricesByItemQuery } from '../features/dashboard/api/dashboardApi';
 import { skipToken } from '@reduxjs/toolkit/query';
+import PriceHistoryChart from '../features/dashboard/ui/PriceHistoryChart';
 
 function HomeContent() {
 	const player = useSelector((state) => state.player);
@@ -22,7 +20,7 @@ function HomeContent() {
 			<BaseLayout>
 				<ContentLayout>
 					<div className='grid grid-rows-3 lg:grid-rows-1 grid-cols-1 md:grid-cols-4 lg:grid-cols-4 gap-2'>
-						<TheChart player={player} item={item} />
+						<PriceHistoryChart player={player} item={item} />
 						<ItemMain prices={prices?.dailyPrices} isFetching={isFetching} />
 						<RecentBuyers isFetching={isFetching} />
 						<RecentSellers isFetching={isFetching} />
