9cf641db7c3eded6d2fafa66c045362d4d4d1bd9
Kevz Macalalag <mc.kvnflrs@gmail.com>
2026-02-26 02:10:47 +0800

    Decoupled Chart Components

--- src/components/TheChart.jsx
+++ src/components/TheChart.jsx
@@ -1,80 +1,12 @@
 import React from 'react';
-import { Bar, BarChart, CartesianGrid, Legend, Tooltip, XAxis, YAxis } from 'recharts';
 import { getDaisyUIColor } from '../utils/colorHelper';
-import TheLoader from './TheLoader';
 import { useGetDailyPricesByItemQuery } from '../features/dashboard/api/dashboardApi';
 import { skipToken } from '@reduxjs/toolkit/query';
 
-function TheChart({ player, item }) {
-	const { data, isLoading } = useGetDailyPricesByItemQuery(
-		item?.id && player?.id ? { itemId: item.id, playerId: player.id } : skipToken,
-	);
-	const basePrice = item?.basePrice;
-
-	const chartData = data?.map((item) => {
-		const diff = item.previousPrice - basePrice;
-
-		return {
-			date: item.date,
-			value: diff,
-			originalPrice: item.previousPrice,
-			fill: diff >= 0 ? getDaisyUIColor('--color-success') : getDaisyUIColor('--color-error'),
-		};
-	});
-
+function TheChart({ children }) {
 	return (
 		<div className='card w-auto h-100 bg-base-100 row-start-1 col-start-1 col-span-full'>
-			<div className='card-body rechart-wrapper'>
-				<h2 className='card-title'>PRICE HISTORY</h2>
-				{isLoading ? (
-					<TheLoader />
-				) : (
-					<BarChart
-						style={{
-							width: '100%',
-							aspectRatio: 1.718,
-							height: '300px',
-						}}
-						responsive
-						data={chartData}
-					>
-						<CartesianGrid
-							stroke={getDaisyUIColor('--color-base-content')}
-							strokeDasharray='5 5'
-						/>
-						<XAxis dataKey='date' stroke={getDaisyUIColor('--color-base-content')} />
-						<YAxis
-							domain={['auto', 'auto']}
-							tickFormatter={(v) => basePrice + v}
-							stroke={getDaisyUIColor('--color-base-content')}
-						/>
-						<Bar
-							dataKey='value'
-							name={'Price Difference'}
-							fill={getDaisyUIColor('--color-base-content')}
-						/>
-						<Legend
-							align='top'
-							wrapperStyle={{
-								backgroundColor: 'transparent',
-								color: '#fff',
-							}}
-						/>
-						<Tooltip
-							cursor={{ fill: 'rgba(255,255,255,0.04)' }}
-							contentStyle={{
-								backgroundColor: getDaisyUIColor('--color-base-200'),
-								border: `1px solid ${getDaisyUIColor('--color-base-200')}`,
-								color: getDaisyUIColor('--color-base-content'),
-							}}
-							wrapperStyle={{
-								width: 'auto',
-								backgroundColor: `${getDaisyUIColor('--color-base-200')}`,
-							}}
-						/>
-					</BarChart>
-				)}
-			</div>
+			<div className='card-body rechart-wrapper'>{children}</div>
 		</div>
 	);
 }
---
+++ src/features/dashboard/ui/PriceHistoryChart.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import TheChart from '../../../components/TheChart';
+import { useGetDailyPricesByItemQuery } from '../api/dashboardApi';
+import { Bar, BarChart, CartesianGrid, Legend, Tooltip, XAxis, YAxis } from 'recharts';
+import TheLoader from '../../../components/TheLoader';
+import { getDaisyUIColor } from '../../../utils/colorHelper';
+import { skipToken } from '@reduxjs/toolkit/query';
+
+function PriceHistoryChart({ item, player }) {
+	const { data, isLoading } = useGetDailyPricesByItemQuery(
+		item?.id && player?.id ? { itemId: item.id, playerId: player.id } : skipToken,
+	);
+	const basePrice = item?.basePrice;
+
+	const chartData = data?.map((item) => {
+		const diff = item.previousPrice - basePrice;
+
+		return {
+			date: item.date,
+			value: diff,
+			originalPrice: item.previousPrice,
+			fill: diff >= 0 ? getDaisyUIColor('--color-success') : getDaisyUIColor('--color-error'),
+		};
+	});
+
+	return (
+		<TheChart>
+			<h2 className='card-title'>PRICE HISTORY</h2>
+			{isLoading ? (
+				<TheLoader />
+			) : (
+				<BarChart
+					style={{
+						width: '100%',
+						aspectRatio: 1.718,
+						height: '300px',
+					}}
+					responsive
+					data={chartData}
+				>
+					<CartesianGrid
+						stroke={getDaisyUIColor('--color-base-content')}
+						strokeDasharray='5 5'
+					/>
+					<XAxis dataKey='date' stroke={getDaisyUIColor('--color-base-content')} />
+					<YAxis
+						domain={['auto', 'auto']}
+						tickFormatter={(v) => basePrice + v}
+						stroke={getDaisyUIColor('--color-base-content')}
+					/>
+					<Bar
+						dataKey='value'
+						name={'Price Difference'}
+						fill={getDaisyUIColor('--color-base-content')}
+					/>
+					<Legend
+						align='top'
+						wrapperStyle={{
+							backgroundColor: 'transparent',
+							color: '#fff',
+						}}
+					/>
+					<Tooltip
+						cursor={{ fill: 'rgba(255,255,255,0.04)' }}
+						contentStyle={{
+							backgroundColor: getDaisyUIColor('--color-base-200'),
+							border: `1px solid ${getDaisyUIColor('--color-base-200')}`,
+							color: getDaisyUIColor('--color-base-content'),
+						}}
+						wrapperStyle={{
+							width: 'auto',
+							backgroundColor: `${getDaisyUIColor('--color-base-200')}`,
+						}}
+					/>
+				</BarChart>
+			)}
+		</TheChart>
+	);
+}
+
+export default PriceHistoryChart;
