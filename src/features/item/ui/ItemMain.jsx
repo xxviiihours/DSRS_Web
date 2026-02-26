be0f6e1589596f9c8b59063d690e716ca8696374
Kevz Macalalag <mc.kvnflrs@gmail.com>
2026-02-27 01:06:05 +0800

    Modify to decouple some functions
    sperated presentation UIs
    Moved actions to a seperate hooks

--- src/features/item/ui/ItemMain.jsx
+++ src/features/item/ui/ItemMain.jsx
@@ -1,41 +1,59 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef } from 'react';
 import TheLoaderInfo from '../../../components/TheLoaderInfo';
-import { useDispatch } from 'react-redux';
-import { setItem } from '../model/itemSlice';
 import image from '../../../assets/images/fantasy_item_6.png';
 import { currencyFormat } from '../../../utils/valueFormatter';
 import TransactionForm from '../../market/ui/TransactionForm';
+import { useItemNavigation } from '../../../hooks/ItemNavigation';
+
+function ItemNavigationSlider({ previousAction, nextAction }) {
+	return (
+		<>
+			{/* Navigation buttons inside the card */}
+			<div className='absolute flex justify-between transform -translate-y-1/2 left-0 right-0 top-1/2 p-2'>
+				<div className='absolute top-1 left-2 right-2 flex justify-between transform -translate-y-1/2'>
+					<button onClick={previousAction} className='btn btn-circle btn-info btn-ghost'>
+						❮
+					</button>
+					<button onClick={nextAction} className='btn btn-circle btn-info btn-ghost'>
+						❯
+					</button>
+				</div>
+			</div>
+		</>
+	);
+}
+
+function ItemInfo({ data, children }) {
+	return (
+		<>
+			<figure>
+				<img src={image} className='w-150' alt='Shoes' />
+			</figure>
+			<div className='card-body'>
+				<h2 className='card-title'>
+					<p className='font-bold'>{currencyFormat(data.price)}</p>
+					{data.state === 0 && <div className='badge badge-success ml-2'>LOW</div>}
+					{data.state === 1 && <div className='badge badge-error ml-2'>HIGH</div>}
+				</h2>
+				<h3 className='opacity-60'>
+					Base Price:{' '}
+					<span className='line-through'>{currencyFormat(data.item.basePrice)}</span>
+				</h3>
+				<h3 className='font-semibold'>{data.item.name}</h3>
+				<p className='italic line-clamp-3'>{data.item.description}</p>
+				<div className='card-actions justify-between mt-4'>{children}</div>
+			</div>
+		</>
+	);
+}
 
 function ItemMain({ prices, isFetching }) {
-	const dispatch = useDispatch();
 	const carouselRef = useRef(null);
-	const [currentSlide, setCurrentSlide] = useState(0);
-	const [quantity, setQuantity] = useState(0);
 
-	const totalSlides = prices?.length;
-
-	const goto = (index) => {
-		if (carouselRef.current) {
-			const width = carouselRef.current.offsetWidth;
-			carouselRef.current.scrollTo({
-				left: width * index,
-				behavior: 'smooth',
-			});
-			setCurrentSlide(index);
-		}
-
-		dispatch(setItem(prices[index].item));
-	};
-
-	const nextItem = () => goto((currentSlide + 1) % totalSlides);
-	const prevItem = () => goto((currentSlide - 1 + totalSlides) % totalSlides);
-
-	useEffect(() => {
-		if (prices?.length > 0) {
-			console.log('triggered');
-			dispatch(setItem(prices[0].item));
-		}
-	}, [prices, dispatch]);
+	const { currentSlide, nextItem, prevItem } = useItemNavigation({
+		data: prices,
+		carouselRef,
+	});
 
 	return (
 		<div
@@ -53,42 +71,13 @@ function ItemMain({ prices, isFetching }) {
 							<TheLoaderInfo />
 						) : (
 							<>
-								<figure>
-									<img src={image} className='w-150' alt='Shoes' />
-								</figure>
-								<div className='card-body'>
-									<h2 className='card-title'>
-										<p className='font-bold'>{currencyFormat(price.price)}</p>
-										{price.state === 0 && <div className='badge badge-success ml-2'>LOW</div>}
-										{price.state === 1 && <div className='badge badge-error ml-2'>HIGH</div>}
-									</h2>
-									<h3 className='opacity-60'>
-										Base Price:{' '}
-										<span className='line-through'>
-											{currencyFormat(price.item.basePrice)}
-										</span>
-									</h3>
-									<h3 className='font-semibold'>{price.item.name}</h3>
-									<p className='italic line-clamp-3'>{price.item.description}</p>
-									<div className='card-actions justify-between mt-4'>
-										{currentSlide === index && <TransactionForm data={price} />}
-									</div>
-								</div>
+								<ItemInfo data={price}>
+									{prices[index] !== null && <TransactionForm data={price} />}
+								</ItemInfo>
 							</>
 						)}
 					</div>
-
-					{/* Navigation buttons inside the card */}
-					<div className='absolute flex justify-between transform -translate-y-1/2 left-0 right-0 top-1/2 p-2'>
-						<div className='absolute top-1 left-2 right-2 flex justify-between transform -translate-y-1/2'>
-							<button onClick={prevItem} className='btn btn-circle btn-info btn-ghost'>
-								❮
-							</button>
-							<button onClick={nextItem} className='btn btn-circle btn-info btn-ghost'>
-								❯
-							</button>
-						</div>
-					</div>
+					<ItemNavigationSlider previousAction={prevItem} nextAction={nextItem} />
 				</div>
 			))}
 		</div>
---
+++ src/hooks/ItemNavigation.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { setItem } from '../features/item/model/itemSlice';
+
+export const useItemNavigation = ({ data, carouselRef }) => {
+	const dispatch = useDispatch();
+
+	const [currentSlide, setCurrentSlide] = useState(0);
+
+	const totalSlides = data?.length || 0;
+
+	const nextItem = () => setCurrentSlide((prev) => (prev + 1) % totalSlides);
+
+	const prevItem = () => setCurrentSlide((prev) => (prev - 1 + totalSlides) % totalSlides);
+
+	useEffect(() => {
+		if (!data?.length) return;
+		if (!carouselRef?.current) return;
+
+		const width = carouselRef.current.offsetWidth;
+
+		carouselRef.current.scrollTo({
+			left: width * currentSlide,
+			behavior: 'smooth',
+		});
+
+		dispatch(setItem(data[currentSlide]?.item));
+	}, [currentSlide, carouselRef, data, dispatch]);
+
+	return { currentSlide, nextItem, prevItem };
+};
