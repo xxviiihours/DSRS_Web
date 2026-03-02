ad3beb546d40e6a56347de8db6087c4e31ea2bd2
Kevz Macalalag <mc.kvnflrs@gmail.com>
2026-03-02 20:12:44 +0800

    Add action handler in 3D Card component

--- src/shared/components/The3DCard.jsx
+++ src/shared/components/The3DCard.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
-function The3DCard({ children }) {
+function The3DCard({ children, action }) {
+	const handleAction = () => {
+		action();
+	};
 	return (
-		<a className='hover-3d m-2 cursor-pointer'>
+		<a className='hover-3d m-2 cursor-pointer' onClick={handleAction}>
 			{children}
 			<div></div>
 			<div></div>
