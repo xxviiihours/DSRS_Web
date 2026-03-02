48bd4d145e93ac0576c1a8a381eea6d112808516
Kevz Macalalag <mc.kvnflrs@gmail.com>
2026-03-02 20:01:27 +0800

    Add market display in Inventory page

---
+++ src/features/market/ui/ItemMarket.jsx
@@ -0,0 +1,53 @@
+import { currencyFormat, The3DCard, TheModal } from '@/shared';
+import React, { useState } from 'react';
+import image from '@/assets/images/fantasy_item_4.png';
+import { PriceHistoryChart } from '@/features/dashboard';
+import { useSelector } from 'react-redux';
+import { TransactionForm } from '@/features/market';
+
+function ItemMarket({ data }) {
+	const player = useSelector((state) => state.player);
+	const [show, setShow] = useState(false);
+	const isPositive = data.percentage >= 0;
+	return (
+		<>
+			<The3DCard
+				action={() => {
+					setShow(true);
+				}}
+			>
+				<div className='card w-auto bg-base-100 card-xs shadow-sm'>
+					<figure>
+						<img src={image} className='w-100' alt='Shoes' />
+					</figure>
+					<div className='card-body'>
+						<h2 className='card-title text-xs text-left mb-2'>{data.item.name} </h2>
+
+						<p className='text-right font-semibold'>
+							{currencyFormat(data.price)}{' '}
+							<span
+								className={`text-xs font-sans text-right ${isPositive ? 'text-success' : 'text-error'}`}
+							>
+								({data.percentage}%)
+							</span>
+						</p>
+
+						<p className='text-right opacity-60'>
+							Original Price:{' '}
+							<span className='line-through'>{currencyFormat(data.item.basePrice)}</span>
+						</p>
+					</div>
+				</div>
+			</The3DCard>
+			<TheModal show={show} onClose={() => setShow(false)} size='large'>
+				<PriceHistoryChart player={player} item={data.item} />
+				<div className='divider'></div>
+				<div className='modal-action'>
+					<TransactionForm data={data} />
+				</div>
+			</TheModal>
+		</>
+	);
+}
+
+export default ItemMarket;
