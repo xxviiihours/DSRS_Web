3071d53da5613474be0b3d793a05c1f2d4b9b242
Kevz Macalalag <mc.kvnflrs@gmail.com>
2026-03-03 20:34:55 +0800

    Adjust inventory display

--- src/pages/InventoryContent.jsx
+++ src/pages/InventoryContent.jsx
@@ -10,13 +10,17 @@ function InventoryContent() {
 	return (
 		<BaseLayout>
 			<ContentLayout>
-				<div className='divider'>Inventory</div>
-				<div className='grid grid-rows-6 lg:grid-rows-1 grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4'>
-					{inventoryItems?.map((item, index) => (
-						<InventoryItem key={index} data={item} />
-					))}
+				<div className='divider font-bold'>Inventory</div>
+				<div className='grid lg:grid-rows-1 grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4 md:h-115 lg:h-110'>
+					{inventoryItems.length > 0 ? (
+						inventoryItems.map((item, index) => <InventoryItem key={index} data={item} />)
+					) : (
+						<div className='grid col-span-full text-center h-115 content-center'>
+							<h6>NO ITEMS AVAILABLE</h6>
+						</div>
+					)}
 				</div>
-				<div className='divider'>Market</div>
+				<div className='divider font-bold'>Market</div>
 				<div className='grid grid-rows-1 lg:grid-rows-1 grid-cols-1 md:grid-cols-6 lg:grid-cols-6 gap-2'>
 					{data?.dailyPrices?.map((dailyPrice, index) => (
 						<ItemMarket key={index} data={dailyPrice} />
