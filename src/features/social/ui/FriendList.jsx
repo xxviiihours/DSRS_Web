77175cc9d0c45de2f5d4e7f948f0cf3626a2b30a
Kevz Macalalag <mc.kvnflrs@gmail.com>
2026-03-05 22:55:52 +0800

    Add Friend List placeholder in Sidebar layout

---
+++ src/features/social/ui/FriendList.jsx
@@ -0,0 +1,28 @@
+import { FriendSearchForm } from '@/features/social';
+import React from 'react';
+
+function FriendList() {
+	return (
+		<div>
+			<FriendSearchForm />
+			{Array.from({ length: 100 }).map((_, index) => (
+				<li key={index + 1} className='list-row grid grid-cols-5 gap-4 p-2'>
+					<div className='col-span-1'>
+						<div className='flex'>
+							<img
+								className='size-10 rounded-box'
+								src='https://img.daisyui.com/images/profile/demo/1@94.webp'
+							/>
+						</div>
+					</div>
+					<div className='list-col-grow col-span-4'>
+						<div>Name</div>
+						<div className='text-xs uppercase font-semibold opacity-60'>Status ??</div>
+					</div>
+				</li>
+			))}
+		</div>
+	);
+}
+
+export default FriendList;
