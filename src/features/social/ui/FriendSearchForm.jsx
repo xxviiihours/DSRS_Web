adf1d6cd36e94109afa7e60a9b0e7d9df2d92822
Kevz Macalalag <mc.kvnflrs@gmail.com>
2026-03-08 23:27:49 +0800

    Add players search

--- src/features/social/ui/FriendSearchForm.jsx
+++ src/features/social/ui/FriendSearchForm.jsx
@@ -1,27 +1,40 @@
+import { useLazyGetPlayersQuery } from '@/features/player';
+import { getApiErrorMessage } from '@/shared';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useFormik } from 'formik';
 import React from 'react';
 
-function FriendSearchForm() {
+function FriendSearchForm({ search }) {
+	const [getPlayers, { isLoading }] = useLazyGetPlayersQuery();
 	const formik = useFormik({
 		initialValues: {
 			search: '',
 		},
+		onSubmit: async (values, { setSubmitting, resetForm }) => {
+			await search(values.search);
+		},
 	});
 
 	return (
-		<label className='input mb-4'>
-			<FontAwesomeIcon icon={faSearch} />
-			<input
-				type='search'
-				className='grow'
-				placeholder='Search'
-				name='search'
-				value={formik.values.search}
-				onChange={formik.handleChange}
-			/>
-		</label>
+		<form onSubmit={formik.handleSubmit}>
+			<div className='join'>
+				<label className='input mb-4'>
+					<FontAwesomeIcon icon={faSearch} />
+					<input
+						type='search'
+						className='grow'
+						placeholder='Search'
+						name='search'
+						value={formik.values.search}
+						onChange={formik.handleChange}
+					/>
+				</label>
+				<button className='btn' type='submit'>
+					search
+				</button>
+			</div>
+		</form>
 	);
 }
 
