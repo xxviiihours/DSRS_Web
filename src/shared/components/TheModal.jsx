c675a6da0cb9c3693539a169be903b18caa603ab
Kevz Macalalag <mc.kvnflrs@gmail.com>
2026-03-13 22:24:22 +0800

    fixed modal size

--- src/shared/components/TheModal.jsx
+++ src/shared/components/TheModal.jsx
@@ -4,7 +4,8 @@ function TheModal({ show, submit = null, onClose, children, size = 'default' }) 
 	return (
 		<dialog id='my_modal_2' className={show ? 'modal modal-open' : 'modal'}>
 			<div
-				className={`modal-box flex flex-col ${size === 'large' ? 'w-11/12 max-w-5xl h-160' : 'h-100 justify-center items-center text-center'}`}
+				className={`modal-box flex flex-col justify-center items-center text-center
+					${size === 'large' ? 'w-11/12 max-w-5xl h-160' : size === 'small' ? 'h-100' : 'h-auto'}`}
 			>
 				{children}
 			</div>
