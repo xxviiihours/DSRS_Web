f3530d92ab261dd129ddd0234cf5bf00248fc27d
Kevz Macalalag <mc.kvnflrs@gmail.com>
2026-03-15 19:35:23 +0800

    custom hooks related to inventory added

---
+++ src/features/inventory/hooks/useCalculateItem.js
@@ -0,0 +1,45 @@
+import { useInitDailyPricesQuery } from '@/features/market';
+import { useSelector } from 'react-redux';
+
+const useCalculateItem = ({ id }) => {
+	const { inventoryItems } = useSelector((state) => state.player);
+	const { data, isLoading, isUninitialized } = useInitDailyPricesQuery(
+		id ? { id: id } : skipToken,
+	);
+
+	const itemDetails = inventoryItems.map((inventory) => {
+		const result = data?.dailyPrices.find((p) => p.item.id === inventory.itemId);
+
+		const profit = result?.price - result?.item.basePrice;
+		const percent = Math.round((profit / result?.price) * 100);
+		return {
+			item: result?.item,
+			currentPrice: result?.price,
+			state: result?.state,
+			quantity: inventory.quantity,
+			profit: profit,
+			percent: percent,
+		};
+	});
+
+	const inventoryValue =
+		inventoryItems.reduce((total, inventory) => {
+			const currentPrice = data?.dailyPrices.find((p) => p.item.id === inventory.itemId);
+
+			return total + (currentPrice?.price ?? 0) * inventory.quantity;
+		}, 0) ?? 0;
+
+	return {
+		data: {
+			itemDetails,
+			inventoryValue,
+			marketDetails: data?.dailyPrices,
+		},
+		state: {
+			isLoading,
+			isUninitialized,
+		},
+	};
+};
+
+export default useCalculateItem;
--- src/features/inventory/index.js
+++ src/features/inventory/index.js
@@ -1,1 +1,3 @@
 export { default as InventoryItem } from './ui/InventoryItem';
+
+export { default as useCalculateItem } from './hooks/useCalculateItem';
