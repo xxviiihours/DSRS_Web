abcb7380748cf6c4072325ccf8f00d29245b8068
Kevz Macalalag <mc.kvnflrs@gmail.com>
2026-03-16 11:42:20 +0800

    applied changeTheme in the header

--- src/shared/components/TheHeader.jsx
+++ src/shared/components/TheHeader.jsx
@@ -1,25 +1,31 @@
 import { clearPlayer } from '@/features/player';
-import { usePlayerSync } from '@/shared';
+import { useThemeProvider } from '@/providers';
 import { currencyFormat } from '@/shared/utils/valueFormatter';
+import { faCartShopping, faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router';
 
 function TheHeader() {
 	const dispatch = useDispatch();
-	const player = useSelector((state) => state.player);
-	const item = useSelector((state) => state.item);
 	const navigate = useNavigate();
 
+	const player = useSelector((state) => state.player);
+	const item = useSelector((state) => state.item);
+
+	const { theme, changeTheme } = useThemeProvider();
+
 	const currentItem = player?.inventoryItems.find((p) => p.itemId === item?.id);
+
 	return (
-		<div className='navbar bg-base-100 shadow-sm flex justify-between gap-2'>
+		<div className='navbar bg-base-100 border-2 border-base-300 flex justify-between gap-2'>
 			<div className='flex flex-1'>
 				<a className='btn btn-ghost text-xl' onClick={() => navigate('/')}>
 					Daily Stock Redistribution System
 				</a>
 			</div>
-			<div className='grid content-center'>
+			<div className='grid content-center mr-4'>
 				<span className='text-xs text-info opacity-60 inline-block align-baseline'>
 					Balance
 				</span>
@@ -27,25 +33,23 @@ function TheHeader() {
 					{currencyFormat(player?.balance ?? 0)}
 				</p>
 			</div>
-			<div className='flex-none'>
+			<div className='flex-none gap-2'>
+				<label className='swap swap-rotate'>
+					{/* this hidden checkbox controls the state */}
+					<input
+						type='checkbox'
+						className='theme-controller'
+						value={theme}
+						checked={theme === 'wight-light'}
+						onChange={(e) => changeTheme(e.target.checked ? 'wight-light' : 'wight')}
+					/>
+					<FontAwesomeIcon className='swap-on fill-current' icon={faMoon} size='lg' />
+					<FontAwesomeIcon className='swap-off fill-current' icon={faSun} size='lg' />
+				</label>
 				<div className='dropdown dropdown-end'>
 					<div tabIndex={0} role='button' className='btn btn-ghost btn-circle'>
 						<div className='indicator'>
-							<svg
-								xmlns='http://www.w3.org/2000/svg'
-								className='h-5 w-5'
-								fill='none'
-								viewBox='0 0 24 24'
-								stroke='currentColor'
-							>
-								{' '}
-								<path
-									strokeLinecap='round'
-									strokeLinejoin='round'
-									strokeWidth='2'
-									d='M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z'
-								/>{' '}
-							</svg>
+							<FontAwesomeIcon icon={faCartShopping} size='xl' />
 							<span className='badge badge-sm indicator-item'>
 								{currentItem?.quantity ?? 0}
 							</span>
