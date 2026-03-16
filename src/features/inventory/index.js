f061325c87b866db50af03a7836bd6ddffbc2797
Kevz Macalalag <mc.kvnflrs@gmail.com>
2026-03-16 23:40:41 +0800

    Add inventory container

--- src/features/inventory/index.js
+++ src/features/inventory/index.js
@@ -1,3 +1,4 @@
+export { default as Inventory } from './ui/Inventory';
 export { default as InventoryItem } from './ui/InventoryItem';
 
 export { default as useCalculateItem } from './hooks/useCalculateItem';
---
+++ src/features/inventory/ui/Inventory.jsx
@@ -0,0 +1,22 @@
+import { InventoryItem } from '@/features/inventory';
+import { currencyFormat } from '@/shared';
+import React from 'react';
+
+function Inventory({ items, value }) {
+	return (
+		<>
+			<h3 className='font-bold'>Your Inventory</h3>
+			<span className='font-bold text-xs opacity-60'>
+				{items.length} {items.length > 1 ? 'Items' : 'Item'} - Total value:{' '}
+				{currencyFormat(value)}
+			</span>
+			<div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4 mt-4 grid-rows-1'>
+				{items.map((item, index) => (
+					<InventoryItem key={index + 1} data={item} />
+				))}
+			</div>
+		</>
+	);
+}
+
+export default Inventory;
