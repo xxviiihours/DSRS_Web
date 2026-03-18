d7676fbba7bb9ba02390c6de9821e725b2f1015b
Kevz Macalalag <mc.kvnflrs@gmail.com>
2026-03-18 20:42:36 +0800

    Added Register Content

---
+++ src/pages/RegisterContent.jsx
@@ -0,0 +1,8 @@
+import { PlayerRegisterForm } from '@/features/player';
+import React from 'react';
+
+function RegisterContent() {
+	return <PlayerRegisterForm />;
+}
+
+export default RegisterContent;
--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import { createBrowserRouter } from 'react-router';
 import { lazy } from 'react';
 
 const LoginContent = lazy(() => import('./pages/LoginContent'));
-
+const RegisterContent = lazy(() => import('./pages/RegisterContent'));
 const AuthGuard = lazy(() => import('./guards/AuthGuard'));
 const HomeContent = lazy(() => import('./pages/HomeContent'));
 const InventoryContent = lazy(() => import('./pages/InventoryContent'));
@@ -12,6 +12,10 @@ export const routes = createBrowserRouter([
 	{
 		path: '/login',
 		Component: LoginContent,
+	},
+	{
+		path: '/register',
+		Component: RegisterContent,
 	},
 	{
 		Component: AuthGuard,
