f08834f4a3061d0a5d25cb625ee91964b1a3b1b6
Kevz Macalalag <mc.kvnflrs@gmail.com>
2026-03-19 20:23:44 +0800

    fixed player stats display on smaller screen

--- src/features/player/ui/PlayerStats.jsx
+++ src/features/player/ui/PlayerStats.jsx
@@ -12,44 +12,46 @@ import React from 'react';
 
 function PlayerStats({ player, value }) {
 	return (
-		<>
-			<div className='stat bg-base-100 col-span-1 rounded-2xl'>
-				<div className='stat-figure text-info'>
-					<FontAwesomeIcon icon={faSackDollar} size='2xl' />
+		<div className='col-span-full'>
+			<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2'>
+				<div className='stat bg-base-100 rounded-2xl'>
+					<div className='stat-figure text-info'>
+						<FontAwesomeIcon icon={faSackDollar} size='2xl' />
+					</div>
+					<div className='stat-title'>Total Balance</div>
+					<div className='stat-value'>{currencyFormat(player?.balance)}</div>
+					<div className='stat-desc text-error opacity-60'>
+						<FontAwesomeIcon icon={faArrowTrendDown} /> 90 (14%)
+					</div>
 				</div>
-				<div className='stat-title'>Total Balance</div>
-				<div className='stat-value'>{currencyFormat(player?.balance)}</div>
-				<div className='stat-desc text-error opacity-60'>
-					<FontAwesomeIcon icon={faArrowTrendDown} /> 90 (14%)
+				<div className='stat bg-base-100 col-span-1 rounded-2xl'>
+					<div className='stat-figure text-info'>
+						<FontAwesomeIcon icon={faMoneyBillTrendUp} size='2xl' />
+					</div>
+					<div className='stat-title'>Total Profit</div>
+					<div className='stat-value'>{currencyFormat(player?.balance)}</div>
+					<div className='stat-desc'>from past 7 sales</div>
+				</div>
+				<div className='stat bg-base-100 col-span-1 rounded-2xl'>
+					<div className='stat-title'>Inventory Value</div>
+					<div className='stat-value'>{currencyFormat(value)}</div>
+					<div className='stat-desc text-success opacity-60'>
+						<FontAwesomeIcon icon={faArrowTrendUp} /> 90 (14%) unrealized
+					</div>
+					<div className='stat-figure text-info col-start-2'>
+						<FontAwesomeIcon icon={faBoxesStacked} size='2xl' />
+					</div>
+				</div>
+				<div className='stat bg-base-100 col-span-1 rounded-2xl'>
+					<div className='stat-figure text-info'>
+						<FontAwesomeIcon icon={faMedal} size='2xl' />
+					</div>
+					<div className='stat-title'>Achievements</div>
+					<div className='stat-value'>4/10</div>
+					<div className='stat-desc text-purple-500 opacity-60'>(66%) completed</div>
 				</div>
 			</div>
-			<div className='stat bg-base-100 col-span-1 rounded-2xl'>
-				<div className='stat-figure text-info'>
-					<FontAwesomeIcon icon={faMoneyBillTrendUp} size='2xl' />
-				</div>
-				<div className='stat-title'>Total Profit</div>
-				<div className='stat-value'>{currencyFormat(player?.balance)}</div>
-				<div className='stat-desc'>from past 7 sales</div>
-			</div>
-			<div className='stat bg-base-100 col-span-1 rounded-2xl'>
-				<div className='stat-figure text-info'>
-					<FontAwesomeIcon icon={faBoxesStacked} size='2xl' />
-				</div>
-				<div className='stat-title'>Inventory Value</div>
-				<div className='stat-value'>{currencyFormat(value)}</div>
-				<div className='stat-desc text-success opacity-60'>
-					<FontAwesomeIcon icon={faArrowTrendUp} /> 90 (14%) unrealized
-				</div>
-			</div>
-			<div className='stat bg-base-100 col-span-1 rounded-2xl'>
-				<div className='stat-figure text-info'>
-					<FontAwesomeIcon icon={faMedal} size='2xl' />
-				</div>
-				<div className='stat-title'>Achievements</div>
-				<div className='stat-value'>4/10</div>
-				<div className='stat-desc text-purple-500 opacity-60'>(66%) completed</div>
-			</div>
-		</>
+		</div>
 	);
 }
 
