47cbf26b08689c8e1ba0bff6a88f98f1f9f9a3ca
Kevz Macalalag <mc.kvnflrs@gmail.com>
2026-03-20 21:03:17 +0800

    Fixed Alert Icon to use FontAwesome instead of manual svg element

--- src/shared/components/TheAlert.jsx
+++ src/shared/components/TheAlert.jsx
@@ -1,4 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import { faCheckCircle, faCircleXmark } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useEffect } from 'react';
 
 const alertStyles = {
 	success: 'alert-success',
@@ -11,49 +13,25 @@ function TheAlert({ show, succeeded, message, onClose }) {
 		if (!show) return;
 
 		const timer = setTimeout(() => {
-			onClose(); // parent sets show = false
-		}, 4000);
+			onClose();
+		}, 3000);
 
 		return () => clearTimeout(timer);
 	}, [show, succeeded, onClose]);
 
-	if (show)
+	if (true)
 		return (
 			<div
 				role='alert'
 				className={`alert ${succeeded ? alertStyles.success : alertStyles.error} fixed top-6 right-6 z-1000 w-96 max-w-[90vw]`}
 			>
 				{succeeded ? (
-					<svg
-						xmlns='http://www.w3.org/2000/svg'
-						className='h-6 w-6 shrink-0 stroke-current text-base-100'
-						fill='none'
-						viewBox='0 0 24 24'
-					>
-						<path
-							strokeLinecap='round'
-							strokeLinejoin='round'
-							strokeWidth='2'
-							d='M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z'
-						/>
-					</svg>
+					<FontAwesomeIcon icon={faCheckCircle} />
 				) : (
-					<svg
-						xmlns='http://www.w3.org/2000/svg'
-						className='h-6 w-6 shrink-0 stroke-current text-base-100'
-						fill='none'
-						viewBox='0 0 24 24'
-					>
-						<path
-							strokeLinecap='round'
-							strokeLinejoin='round'
-							strokeWidth='2'
-							d='M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z'
-						/>
-					</svg>
+					<FontAwesomeIcon icon={faCircleXmark} />
 				)}
 
-				<span className='text-base-100'>{message}</span>
+				<span className='text-white'>{message}</span>
 			</div>
 		);
 }
