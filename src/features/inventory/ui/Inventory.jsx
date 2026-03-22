8e6088c1b9bd6f08a5fb63c3ee3498a465ca19bb
Kevz Macalalag <mc.kvnflrs@gmail.com>
2026-03-22 20:38:10 +0800

    some changes regarding display value

--- src/features/inventory/ui/Inventory.jsx
+++ src/features/inventory/ui/Inventory.jsx
@@ -2,13 +2,14 @@ import { InventoryItem } from '@/features/inventory';
 import { currencyFormat } from '@/shared';
 import React from 'react';
 
-function Inventory({ items, value }) {
+function Inventory({ items, inventoryDetails }) {
+	console.log(inventoryDetails);
 	return (
 		<div className='p-3'>
 			<h3 className='font-bold'>Your Inventory</h3>
 			<span className='font-bold text-xs opacity-60'>
 				{items.length} {items.length > 1 ? 'Items' : 'Item'} - Total value:{' '}
-				{currencyFormat(value)}
+				{currencyFormat(inventoryDetails.totalValue)}
 			</span>
 			<div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4 mt-4 grid-rows-1'>
 				{items.map((item, index) => (
