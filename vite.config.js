13f7954df5e73e00429401ec3cb769853023a229
Kevz Macalalag <mc.kvnflrs@gmail.com>
2026-04-13 19:07:20 +0800

    removed axios from the package added react compiler to automatically utilize memoization instead of manually adding it each components

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,15 @@ import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
+	plugins: [
+		react({
+			babel: {
+				plugins: ['babel-plugin-react-compiler'],
+			},
+		}),
+		,
+		tailwindcss(),
+	],
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
